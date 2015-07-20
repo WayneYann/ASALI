4e1788c2b79e994f47db14cf07afd1561e17f7aa
srebughini <ste.rebu@gmail.com>
2015-07-20 22:07:40 +0200

    Add examples of the input files

---
+++ input/heterogeneousPFR.sh
@@ -0,0 +1,121 @@
+touch heterogeneousPFR.txt
+
+echo '/*##############################################################################################' >> heterogeneousPFR.txt
+echo '#                                                                                              #' >> heterogeneousPFR.txt
+echo '#     #############       #############       #############       ####                ####     #' >> heterogeneousPFR.txt
+echo '#    #             #     #             #     #             #     #    #              #    #    #' >> heterogeneousPFR.txt
+echo '#    #    #####    #     #    #########      #    #####    #     #    #              #    #    #' >> heterogeneousPFR.txt
+echo '#    #    #   #    #     #    #              #    #   #    #     #    #              #    #    #' >> heterogeneousPFR.txt
+echo '#    #    #####    #     #    #              #    #####    #     #    #              #    #    #' >> heterogeneousPFR.txt
+echo '#    #             #     #    #########      #             #     #    #              #    #    #' >> heterogeneousPFR.txt
+echo '#    #             #     #             #     #             #     #    #              #    #    #' >> heterogeneousPFR.txt
+echo '#    #    #####    #      #########    #     #    #####    #     #    #              #    #    #' >> heterogeneousPFR.txt
+echo '#    #    #   #    #              #    #     #    #   #    #     #    #              #    #    #' >> heterogeneousPFR.txt
+echo '#    #    #   #    #      #########    #     #    #   #    #     #    #########      #    #    #' >> heterogeneousPFR.txt
+echo '#    #    #   #    #     #             #     #    #   #    #     #             #     #    #    #' >> heterogeneousPFR.txt
+echo '#     ####     ####       #############       ####     ####       #############       ####     #' >> heterogeneousPFR.txt
+echo '#                                                                                              #' >> heterogeneousPFR.txt
+echo '#   Department of Energy                                                                       #' >> heterogeneousPFR.txt
+echo '#   Politecnico di Milano                                                                      #' >> heterogeneousPFR.txt
+echo '#   Author: Stefano Rebughini <stefano.rebughini@polimi.it>                                    #' >> heterogeneousPFR.txt
+echo '#                                                                                              #' >> heterogeneousPFR.txt
+echo '################################################################################################' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo 'Kinetics path           CH4_UBI/kinetics' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo 'Volumetric flow rate    10 Nl/min' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo 'Pressure                1 bar' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo 'Temperature' >> heterogeneousPFR.txt
+echo '{' >> heterogeneousPFR.txt
+echo '  solid                 500 °C' >> heterogeneousPFR.txt
+echo '  gas                    40 °C' >> heterogeneousPFR.txt
+echo '}' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo 'Reactor' >> heterogeneousPFR.txt
+echo '{' >> heterogeneousPFR.txt
+echo '  inert length          0.5  cm' >> heterogeneousPFR.txt
+echo '  catalytic length      26   mm' >> heterogeneousPFR.txt
+echo '  hydraulic diameter    24   mm' >> heterogeneousPFR.txt
+echo '  channel diameter      1.1  mm' >> heterogeneousPFR.txt
+echo '  channel shape         square' >> heterogeneousPFR.txt
+echo '  void fraction         0.75' >> heterogeneousPFR.txt
+echo '  external limitations  massTransfer' >> heterogeneousPFR.txt
+echo '}' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo 'Catalyst' >> heterogeneousPFR.txt
+echo '{' >> heterogeneousPFR.txt
+echo '  mass                  500 mg' >> heterogeneousPFR.txt
+echo '  dispersion            0.2' >> heterogeneousPFR.txt
+echo '  Rh fraction           0.02' >> heterogeneousPFR.txt
+echo '  deactivation factor   1.0' >> heterogeneousPFR.txt
+echo '}' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo 'Solid' >> heterogeneousPFR.txt
+echo '{' >> heterogeneousPFR.txt
+echo '  density               2300 Kg/m3' >> heterogeneousPFR.txt
+echo '  conductivity          2.5  W/m/K' >> heterogeneousPFR.txt
+echo '  specific heat         925 J/Kg/K' >> heterogeneousPFR.txt
+echo '} ' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo 'Mole fractions' >> heterogeneousPFR.txt
+echo '{' >> heterogeneousPFR.txt
+echo '  CH4                   0.27' >> heterogeneousPFR.txt
+echo '  O2                    0.1593' >> heterogeneousPFR.txt
+echo '  N2                    0.5707' >> heterogeneousPFR.txt
+echo '  inert                 N2' >> heterogeneousPFR.txt
+echo '}' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo 'Solver options' >> heterogeneousPFR.txt
+echo '{' >> heterogeneousPFR.txt
+echo '  Constraints          false' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo '  Absolute tollerance' >> heterogeneousPFR.txt
+echo '  (' >> heterogeneousPFR.txt
+echo '    specie           1e-15' >> heterogeneousPFR.txt
+echo '    temperature      1e-08' >> heterogeneousPFR.txt
+echo '  )' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo '  Relative tollerance' >> heterogeneousPFR.txt
+echo '  (' >> heterogeneousPFR.txt
+echo '    specie           1e-08' >> heterogeneousPFR.txt
+echo '    temperature      1e-08' >> heterogeneousPFR.txt
+echo '  )' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo '  Energy equation    on' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo '  Reactions' >> heterogeneousPFR.txt
+echo '  (' >> heterogeneousPFR.txt
+echo '    Homogeneous      off' >> heterogeneousPFR.txt
+echo '    Heterogeneous    on' >> heterogeneousPFR.txt
+echo '  )' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo '  Grid' >> heterogeneousPFR.txt
+echo '  (' >> heterogeneousPFR.txt
+echo '    Growing                   on' >> heterogeneousPFR.txt
+echo '    Minimum number of points  10' >> heterogeneousPFR.txt
+echo '    Maximum number of points  800' >> heterogeneousPFR.txt
+echo '    Add number of points      300' >> heterogeneousPFR.txt
+echo '    Resolution type           new' >> heterogeneousPFR.txt
+echo '    Discretization scheme     BDS' >> heterogeneousPFR.txt
+echo '  )' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo '  Diffusion in gas off' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo '  Accepted errors' >> heterogeneousPFR.txt
+echo '  (' >> heterogeneousPFR.txt
+echo '    grid               1e-08' >> heterogeneousPFR.txt
+echo '    energy             1e-05' >> heterogeneousPFR.txt
+echo '  )' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo '  Integration time            100 s' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo '  Results                     mole' >> heterogeneousPFR.txt
+echo '}' >> heterogeneousPFR.txt
+echo ' ' >> heterogeneousPFR.txt
+echo 'Numerical solvers' >> heterogeneousPFR.txt
+echo '{' >> heterogeneousPFR.txt
+echo '  ODE       BzzMath' >> heterogeneousPFR.txt
+echo '  DAE       BzzMath' >> heterogeneousPFR.txt
+echo '}' >> heterogeneousPFR.txt
---
+++ input/idealBATCH.sh
@@ -0,0 +1,81 @@
+touch idealBATCH.txt
+
+echo '/*##############################################################################################' >> idealBATCH.txt
+echo '#                                                                                              #' >> idealBATCH.txt
+echo '#     #############       #############       #############       ####                ####     #' >> idealBATCH.txt
+echo '#    #             #     #             #     #             #     #    #              #    #    #' >> idealBATCH.txt
+echo '#    #    #####    #     #    #########      #    #####    #     #    #              #    #    #' >> idealBATCH.txt
+echo '#    #    #   #    #     #    #              #    #   #    #     #    #              #    #    #' >> idealBATCH.txt
+echo '#    #    #####    #     #    #              #    #####    #     #    #              #    #    #' >> idealBATCH.txt
+echo '#    #             #     #    #########      #             #     #    #              #    #    #' >> idealBATCH.txt
+echo '#    #             #     #             #     #             #     #    #              #    #    #' >> idealBATCH.txt
+echo '#    #    #####    #      #########    #     #    #####    #     #    #              #    #    #' >> idealBATCH.txt
+echo '#    #    #   #    #              #    #     #    #   #    #     #    #              #    #    #' >> idealBATCH.txt
+echo '#    #    #   #    #      #########    #     #    #   #    #     #    #########      #    #    #' >> idealBATCH.txt
+echo '#    #    #   #    #     #             #     #    #   #    #     #             #     #    #    #' >> idealBATCH.txt
+echo '#     ####     ####       #############       ####     ####       #############       ####     #' >> idealBATCH.txt
+echo '#                                                                                              #' >> idealBATCH.txt
+echo '#   Department of Energy                                                                       #' >> idealBATCH.txt
+echo '#   Politecnico di Milano                                                                      #' >> idealBATCH.txt
+echo '#   Author: Stefano Rebughini <stefano.rebughini@polimi.it>                                    #' >> idealBATCH.txt
+echo '#                                                                                              #' >> idealBATCH.txt
+echo '################################################################################################' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo 'Kinetics path CH4_UBI/kinetics' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo 'Pressure    1 atm' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo 'Temperature 1097.15 K' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo 'Volume      5.4e-08 m3' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo 'Catalyst' >> idealBATCH.txt
+echo '{' >> idealBATCH.txt
+echo '	alfa	14630	1/m' >> idealBATCH.txt
+echo '	area	6.007495e-05 m2' >> idealBATCH.txt
+echo '}' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo 'Mole fractions' >> idealBATCH.txt
+echo '{' >> idealBATCH.txt
+echo '  CH4                   0.27' >> idealBATCH.txt
+echo '  O2                    0.1593' >> idealBATCH.txt
+echo '  N2                    0.5707' >> idealBATCH.txt
+echo '  inert                 N2' >> idealBATCH.txt
+echo '}' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo 'Coverage' >> idealBATCH.txt
+echo '{' >> idealBATCH.txt
+echo '	Rh(s)  1.0' >> idealBATCH.txt
+echo '}' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo 'Solver options' >> idealBATCH.txt
+echo '{' >> idealBATCH.txt
+echo '  Constraints          false' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo '  Absolute tollerance' >> idealBATCH.txt
+echo '  (' >> idealBATCH.txt
+echo '    specie           1e-15' >> idealBATCH.txt
+echo '    temperature      1e-08' >> idealBATCH.txt
+echo '  )' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo '  Relative tollerance' >> idealBATCH.txt
+echo '  (' >> idealBATCH.txt
+echo '    specie           1e-08' >> idealBATCH.txt
+echo '    temperature      1e-08' >> idealBATCH.txt
+echo '  )' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo '  Energy equation    on' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo '  Reactions' >> idealBATCH.txt
+echo '  (' >> idealBATCH.txt
+echo '    Homogeneous      off' >> idealBATCH.txt
+echo '    Heterogeneous    on' >> idealBATCH.txt
+echo '  )' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo '  Integration time            100 s' >> idealBATCH.txt
+echo '}' >> idealBATCH.txt
+echo ' ' >> idealBATCH.txt
+echo 'Numerical solvers' >> idealBATCH.txt
+echo '{' >> idealBATCH.txt
+echo '     ODE OpenSMOKE' >> idealBATCH.txt
+echo '}' >> idealBATCH.txt
